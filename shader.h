785ff0adda39b4a05282dd4d17565bec4d100532
Weijuan Cao <wjcao@hdu.edu.cn>
2022-10-13 15:53:25 +0800

    shader header file

---
+++ shader.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include <GL/glew.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE)
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char* message = new char[length];
+		glGetShaderInfoLog(id, length, &length, message);
+		std::cout << "Failed to compile " <<
+			(type == GL_VERTEX_SHADER ? "vertex" : "fragment")
+			<< " shader!" << std::endl;
+		std::cout << message << std::endl;
+		glDeleteShader(id);
+		return 0;
+	}
+
+	return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
+
+static void ParseShader(const std::string& filePath, std::string& vshader, std::string& fshader)
+{
+	std::ifstream stream(filePath);
+
+	std::string line;
+	std::stringstream ss[2];
+	int type;
+	while (getline(stream, line))
+	{
+		if (line.find("#shader") != std::string::npos)
+		{
+			if (line.find("vertex") != std::string::npos)
+				type = 0;
+			else if (line.find("fragment") != std::string::npos)
+				type = 1;
+		}
+		else
+		{
+			ss[type] << line << "\n";
+		}
+	}
+	vshader = ss[0].str();
+	fshader = ss[1].str();
+}
